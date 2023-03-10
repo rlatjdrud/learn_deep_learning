ac76d017e20d97b64867871a620faf0694f8d013
rlatjdrud <jkkim1933@naver.com>
2023-03-10 19:31:34 +0900

    perceptron

---
+++ perceptron/nand.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import numpy as np
+
+def NAND(x1,x2) : 
+	x = np.array([x1,x2])
+	w = np.array([-0.5,-0.5])
+	b = 0.7
+	tmp=np.sum(x*w)+b
+	if(tmp >=0) : 
+		return 1
+	else : 
+		return 0
+
+print(NAND(0,0))
+print(NAND(1,0))
+print(NAND(0,1))
+print(NAND(1,1))
